4f6ebc21d8a1e1e79ecb080f3ab44ee4c2480427
Martel <martel.mosqueda@alumnos.udg.mx>
2020-10-29 22:50:42 -0600

    Limites de Y
    Se ha implementado una función que no permite a los jugadores salir de los límites en Y *Arreglar bug*

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,7 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [HideInInspector]
+    private float MAX_UP = 3.7f;
+    [HideInInspector]
+    private float MIN_DOWN = -3.7f;
+    [HideInInspector]
+    private Vector3 MinRecoil = new Vector3(0, 0.05f, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,14 +19,22 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.name == "Player1")
+        DeterminatePlayerMovement();
+    }
+
+    /// <summary>
+    ///     Determinates which player is going to move
+    /// </summary>
+    private void DeterminatePlayerMovement()
+    {
+        if (gameObject.name == Tags.PLAYER_1)
         {
             if (Input.GetKey(KeyCode.W))
                 MovePlayer();
             else if (Input.GetKey(KeyCode.S))
                 MovePlayer(false);
         }
-        else
+        else   
         {
             if (Input.GetKey(KeyCode.UpArrow))
                 MovePlayer();
@@ -28,13 +44,33 @@ public class PlayerControl : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    ///     Determinates if the player is out of bounds based on
+    ///     its Y coord. There is a Min and Max value for this.
     /// </summary>
-    /// <param name="isUp"></param>
+    /// <returns>   True if the player is out of bounds. </returns>
+    private bool IsPlayerOutOfBounds()
+    {
+        Vector3 currentPos = gameObject.transform.position;
+
+        if (currentPos.y > MAX_UP || currentPos.y < MIN_DOWN)
+            return true;
+
+        return false;
+    }
+
+    /// <summary>
+    ///     Moves the current player up or down
+    /// </summary>
+    /// <param name="isUp"> Are we moving up?   </param>
     private void MovePlayer(bool isUp=true)
     {
+        Vector3 recoil = (isUp) ? -MinRecoil : MinRecoil;
         Vector3 dir = (isUp) ? Vector3.up : Vector3.down;
         dir *= Time.deltaTime * 2;
-        gameObject.transform.position += dir;
+
+        if (!IsPlayerOutOfBounds())
+            gameObject.transform.position += dir;
+        else
+            gameObject.transform.position += recoil;
     }
 }
--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -7,8 +7,10 @@ public class Tags
 {
     // String needed on switches and things like that :D
     public const string BALL = "Ball";
-    public const string PLAYER = "Player";
     public const string LEFT_WALL = "LeftWall";
+    public const string PLAYER = "Player";
+    public const string PLAYER_1 = "Player1";
+    public const string PLAYER_2 = "Player2";
     public const string RIGHT_WALL = "RightWall";
 }
 
