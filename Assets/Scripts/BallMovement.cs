ce0389887b5ffd0a22112393d507bc0249be36e3
mrDock21 <danes_warrior@hotmail.com>
2020-10-29 19:27:17 -0600

    Agregado assets del proyecto

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [Tooltip("Ball speed")]
+    public Vector2 Speed = new Vector2(1, 1);
+    [Tooltip("Ball initial position 3D Vector")]
+    public Vector3 InitialPosition = new Vector3(0, 0, -8);
+    [Tooltip("Player one score text")]
+    public TextMeshProUGUI Player1Text;
+    [Tooltip("Player two score text")]
+    public TextMeshProUGUI Player2Text;
+
+    private Vector3 BallDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ball initial position
+        gameObject.transform.position = InitialPosition;
+        // new direction
+        BallDirection = new Vector3(1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += BallDirection * Time.deltaTime * 2;
+    }
+
+    /// <summary>
+    ///     Changes the ball direction
+    /// </summary>
+    public void ChangeBallDirection(bool isYAxis=true)
+    {
+        if (isYAxis)
+            BallDirection.y *= -1;
+        else
+            BallDirection.x *= -1;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void UpdateScore(bool isPlayer1=true)
+    {
+        string text = (isPlayer1) ? Player1Text.text : Player2Text.text;
+        
+        // ball initial position
+        gameObject.transform.position = InitialPosition;
+        // also change direction 
+        ChangeBallDirection(false);
+
+        // update score text
+        if (isPlayer1)
+            Player1Text.text = string.Format("{0:00}", int.Parse(text) + 1);
+        else
+            Player2Text.text = string.Format("{0:00}", int.Parse(text) + 1);
+    }
+}
---
+++ Assets/Scripts/CollitionDetection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollitionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    ///     Detects the collition with the ball on a wall
+    /// </summary>
+    /// <param name="collision"></param>
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            switch (gameObject.tag)
+            {
+                case "Player":
+                    collision.gameObject.GetComponent<BallMovement>().ChangeBallDirection(false);
+                    break;
+                case "LeftWall":
+                    collision.gameObject.GetComponent<BallMovement>().UpdateScore(false);
+                    break;
+                case "RightWall":
+                    collision.gameObject.GetComponent<BallMovement>().UpdateScore();
+                    break;
+                default:
+                    collision.gameObject.GetComponent<BallMovement>().ChangeBallDirection();
+                    break;
+            } 
+        }
+    }
+}
