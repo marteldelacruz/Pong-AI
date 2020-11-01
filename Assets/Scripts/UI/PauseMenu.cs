05bfef6af6cecaa0d25ef70231d680cdc570a7fb
mrDock21 <danes_warrior@hotmail.com>
2020-10-31 18:33:06 -0600

    Agregada funcionalidad básica de los botones

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PlayerVsPlayerBtn;
+    public GameObject PlayerVsAiBtn;
+    public GameObject AiVsAiBtn;
+
+    public void SwitchPlayers()
+    {
+        bool isBtnShown = !PlayerVsPlayerBtn.activeSelf;
+        PlayerVsPlayerBtn.SetActive(isBtnShown);
+        PlayerVsAiBtn.SetActive(isBtnShown);
+        AiVsAiBtn.SetActive(isBtnShown);
+    }
+
+
+    public void TrainAI()
+    {
+        Debug.Log("<size=20>[DEBUG]  Start training!</size>");
+    }
+
+    public void StartRecording()
+    {
+        Debug.Log("<size=20>[DEBUG]  Recording....</size>");
+    }
+}
---
+++ Assets/Scripts/UI/TextButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextButton : MonoBehaviour
+{
+    public Color HighlightedColor;
+
+    private TextMeshProUGUI text;
+    private Color oldColor;
+
+    private void Start()
+    {
+        text = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        oldColor = text.color;
+    }
+
+    /// <summary>
+    ///     Changes text color to given color
+    /// </summary>
+    public void Highlight()
+    {
+        text.color = HighlightedColor;
+    }
+
+    /// <summary>
+    ///     Resets color to normal
+    /// </summary>
+    public void Unhighlight()
+    {
+        text.color = oldColor;
+    }
+}
